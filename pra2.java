02865a6bfcabdfbdf72425aab03f3cd8f553946e
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2023-10-05 10:20:01 +0900

    pra2.javaの出力処理内容の変更
    Hello　World の出力回数を1から100回に変更

--- pra2.java
+++ pra2.java
@@ -1,7 +1,11 @@
 class pra2{
-public static void main (String[] args){
-        System.out.println("Hello WOrld");
-        }
-        }
+	public static void main (String[] args){
+        	int n = 0;
+		while(n<100){
+		System.out.println("Hello WOrld");
+        	n++;
+		}
+	}
+}
 
 
